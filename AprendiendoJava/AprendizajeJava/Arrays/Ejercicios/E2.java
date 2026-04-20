0c3ecf1465480b83bf0bf0203fd880b06011632a
Rodni0101 <rodnialexanderpuertas@gmail.com>
2026-04-19 19:42:38 -0500

    Arreglos

--- AprendiendoJava/AprendizajeJava/Arrays/Ejercicios/E2.java
+++ AprendiendoJava/AprendizajeJava/Arrays/Ejercicios/E2.java
@@ -1,39 +1,40 @@
 public class E2 {
     public static void main(String[] args) {
-        int [] numeros = {1,2,3,4,5};
-
         int suma = 0;
-        int mayor = numeros[0];
-        int menor = numeros[0];
         int pares = 0;
         int impares = 0;
+        int [] numeros = {1,2,3,4,5,6,7,8,9,10};
+        //si vamos a referirnos a mayor o menor siempre definir despues del arreglo
+        int mayor = numeros[0];
+        int menor = numeros[0];
 
         for(int v = 0; v < numeros.length; v++){
             System.out.println("Posicion = " + v + " Valor = " + numeros[v]);
-            
+
+            if(numeros[v] % 2 == 0){
+                pares ++;
+            }else{
+                impares++;
+            }
+
             suma += numeros[v];
-            
+
             if(numeros[v] > mayor){
                 mayor = numeros[v];
             }
 
-            if(numeros[v] < menor){
+            if (numeros[v] < menor){
                 menor = numeros[v];
-            }
-
-            if(numeros[v] % 2 == 0){
-                pares++;
-            }else{
-                impares++;
             }
         }
 
         double promedio = (double) suma / numeros.length;
         System.out.println("Promedio del arreglo = " + promedio);
-        System.out.println("Suma total de los valores = " + suma);
-        System.out.println("Mayor = " + mayor);
-        System.out.println("Menor = " + menor);
+        System.out.println("Suma total del arreglo = " + suma);
         System.out.println("Pares = " + pares);
         System.out.println("Impares = " + impares);
+        System.out.println("Numero mayor = " + mayor);
+        System.out.println("Numero menor: " + menor);
+
     }
 }
