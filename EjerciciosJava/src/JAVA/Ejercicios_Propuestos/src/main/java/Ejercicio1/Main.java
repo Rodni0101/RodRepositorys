6e7126037b3600d922f74683612383a610865654
Rodni0101 <rodnialexanderpuertas@gmail.com>
2026-04-19 19:43:26 -0500

    Correciones

--- EjerciciosJava/src/JAVA/Ejercicios_Propuestos/src/main/java/Ejercicio1/Main.java
+++ EjerciciosJava/src/JAVA/Ejercicios_Propuestos/src/main/java/Ejercicio1/Main.java
@@ -1,9 +1,10 @@
 package Ejercicio1;
-import java.util.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         var Pedido1 = new Producto();
+        leer.close();
         System.out.print("Ingresa el ID del pedido: ");
         Pedido1.setID_Pedido(leer.nextInt());
         System.out.print("Digite el precio del producto: ");
