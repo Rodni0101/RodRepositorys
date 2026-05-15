6e5e5615ed0d1384bf208cfc32f4cf38ae2a5b5d
Rodni0101 <rodnialexanderpuertas@gmail.com>
2026-05-15 14:55:10 -0500

    Clase viernes

---
+++ JavaScript/EJERCICIOS-Switch/switch.js
@@ -0,0 +1,17 @@
+let valor;
+
+valor = parseInt((prompt("Ingresa un valor entre 1 - 3")))
+switch (valor) {
+    case 1:
+        document.write("Holi")
+        break;
+
+
+    case 2:
+        document.write("Hola")
+        break;
+
+    case 3:
+        document.write("HOLO")
+        break;
+}
