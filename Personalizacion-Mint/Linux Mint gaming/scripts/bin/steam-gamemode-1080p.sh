aa064ba5dd274181afd521f7f960d68938add5b1
Rodni0101 <rodnialexanderpuertas@gmail.com>
2026-05-24 22:12:33 -0500

    Actualizacion

--- Personalizacion-Mint/Linux Mint gaming/scripts/bin/steam-gamemode-1080p.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gamescope -w 1920 -h 1080 -W 1920 -H 1080 -f -- steam -gamepadui
-
-#Optimización para nvidia:
-#gamescope -e --prefer-vk-device --disable-color-management -- steam -gamepadui
-
-#si hay tearing o stutter
-#__GL_SYNC_TO_VBLANK=1 gamescope -e -- steam -gamepadui
